fcfab2332ede3c21e68026df8498d2944a2ef45f
NedimKurtulus <nedimkrtls7@gmail.com>
2025-03-19 17:05:27 +0300

    Create tuple.py

---
+++ Python/tuple.py
@@ -0,0 +1,20 @@
+tupleListe = (2,4,6,"ankara",(2,3,4),[])
+liste = [2,4,6,"ankara",[2,3,4],()]
+
+liste[0]=6
+
+tupleDeger = ("engin",)
+print(type(tupleDeger))
+
+print(tupleListe[1:2])
+print(liste[1:2])
+
+print(tupleListe[-2])
+print(liste[-2])
+
+print(type(tupleListe))
+print(type(liste))
+print(tupleListe)
+print(liste)
+print(len(tupleListe))
+print(len(liste))
