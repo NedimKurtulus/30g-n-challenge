cd9c6aafdf7bb2303c5d829d1661d72f762b2b7e
NedimKurtulus <nedimkrtls7@gmail.com>
2025-03-19 17:26:39 +0300

    Create sets.py

---
+++ Python/sets.py
@@ -0,0 +1,17 @@
+studentsSets = {"engin","derin","salih","ahmet"}
+studentsSets = set("mehmet","veli","ayşe")
+print(studentsSets)
+
+for student in studentsSet:
+  print(student)
+
+print("derin" in studendsSet)
+if "derin" in studentsSet:
+  print("listede var")
+studentsSets.add("ali")
+studentsSets.update(["merve","mert","selin"])
+print(len(sutudentSets))
+studentsSets.remove("salih")
+studentsSet.discard("selin")
+x = studentsSets.clear()
+del studentsSet
