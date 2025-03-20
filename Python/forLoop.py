0d2dbb8888138b29853a4f4ecdf3648d743a2c3d
NedimKurtulus <nedimkrtls7@gmail.com>
2025-03-20 17:41:50 +0300

    Create forLoop.py

---
+++ Python/forLoop.py
@@ -0,0 +1,10 @@
+sehirler = ["ankara","istanbul","izmir"]
+
+for sehir in sehirler:
+  if sehir == "istanbul"
+      continue
+  print(sehir + " için kod = " + sehir[0:3])
+  print(*******)
+
+for x in range(2,100,2):
+  print(x)
