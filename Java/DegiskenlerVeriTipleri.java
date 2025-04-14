b99ae3785d98c7ac03a606303ac07daae0dcbe3a
NedimKurtulus <nedimkrtls7@gmail.com>
2025-04-14 16:36:25 +0300

    Create DegiskenlerVeriTipleri.java

---
+++ Java/DegiskenlerVeriTipleri.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void Main ( String args[] ) {
+    int ogrenciSayisi = 10;
+    String mesaj = "öğrenci sayisi";
+    System.out.println (mesaj + ogrenciSayisi);
+  }
+}
+    
